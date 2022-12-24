276528a14f8040c013a39309163888527f4c04b5
Murad Alwan <free1soft@gmail.com>
2022-12-24 10:59:11 +0300

    projects view is ready

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register web routes for ur application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::view('/layout/projects', 'projects.index');
+Route::view('/layout/projects/create', 'projects.create');
+Route::view('/layout/projects/show', 'projects.show');
